60c87834b8c5532e933b029b09bf140c9c861871
Nandita-Naik <U02CS24S0085@klebcadwd.com>
2025-05-06 16:34:53 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,51 @@
+const products = [
+    {
+        id:1,
+        title:"Wireless Headphones",
+        price: 1999,
+        description:"Noise Cancelling over-ear Bluetooth Headphones",
+        category:"Electronics",
+        image:"images/headphones.png",
+    },
+    {
+        id:2,
+        title:"Smart Watch",
+        price: 2999,
+        description:"Fitness Smart Watch with Heart Rate Monitor",
+        category:"Electronics",
+        image:"images/smartwatch.png",
+    },
+    {
+        id:3,
+        title:"T-shirt",
+        price: 499,
+        description:"100% Cotton Printed T-shirt",
+        category:"Clothing",
+        image:"images/t-shirt.png"
+    },
+    {
+        id:4,
+        title:"Laptop",
+        price: 49999,
+        description:"16 GB RAM and 512 SSD Memory with great processor",
+        category:"Electronics",
+        image:"images/laptop.png"
+    },
+    {
+        id:5,
+        title:"Shoes",
+        price: 799,
+        description:"Comfortable to wear",
+        category:"Footwear",
+        image:"images/shoes.png"
+    },
+    {
+        id:6,
+        title:"Smartphone",
+        price: 19999,
+        description:"8 GB RAM and 128 GB ROM Memory",
+        category:"Electronics",
+        image:"images/smartphone.png"
+    },
+    // Add more products as needed
+];
